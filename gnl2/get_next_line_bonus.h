d1807b9323ff43ae965dfcf1b439e3363df4b8b1
aymen elarrouji <99297879+aymane5554@users.noreply.github.com>
2024-12-21 15:54:12 +0100

    ddd

---
+++ gnl2/get_next_line_bonus.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ayel-arr <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/21 11:11:28 by ayel-arr          #+#    #+#             */
+/*   Updated: 2024/11/21 11:11:34 by ayel-arr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <unistd.h>
+# include <fcntl.h>
+# include <stdlib.h>
+# include <limits.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 5
+# endif
+
+char	*get_next_line(int fd);
+char	*ft_strjoin(char const *s1, char const *s2);
+int		isin(char s[], int size, char c);
+char	*ft_strdup(char *src);
+size_t	ft_strlen(const char *str);
+#endif
